b04589b57458cae473ecab077c60082367908197
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-12-22 23:17:56 +0530

    play2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 import "./App.css";
 // import CodeSplit from "./InterviewPrep/CodeSplit/CodeSplit";
 import React, { Suspense } from 'react';
+import TableExample from "./TableExample";
 function App() {
 
   return (
     <div className="App">
-  
+    <TableExample/>
     </div>
   );
 }
---
+++ src/TableExample.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const TableExample = () => {
+  return (
+    <table border="1" style={{ borderCollapse: "collapse", width: "100%" }}>
+      <thead>
+        <tr>
+          <th>Header 1</th>
+          <th>Header 2</th>
+          <th>Header 3</th>
+          <th>Header 4</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Row 1, Cell 1</td>
+          <td colSpan="2" style={{ textAlign: "center" }}>Merged Cell (spanning 2 columns)</td>
+          <td>Row 1, Cell 4</td>
+        </tr>
+        <tr>
+          <td>Row 2, Cell 1</td>
+          <td>Row 2, Cell 2</td>
+          <td>Row 2, Cell 3</td>
+          <td>Row 2, Cell 4</td>
+        </tr>
+        <tr>
+          <td colSpan="3" style={{ textAlign: "center" }}>Merged Cell (spanning 3 columns)</td>
+          <td>Row 3, Cell 4</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default TableExample;
