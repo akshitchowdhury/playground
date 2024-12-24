87382984ab476e64394406a3276353dd966554b6
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-12-25 00:16:16 +0530

    play5

--- src/TableExample.jsx
+++ src/TableExample.jsx
@@ -12,6 +12,7 @@ const TableExample = () => {
           <th>Header 2</th>
           <th>Header 3</th>
           <th>Header 4</th>
+          <th>Header 5</th>
         </tr>
       </thead>
       <tbody>
