cae1acbbab64d103c4601a6de62b0eb446053616
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-06 22:11:47 +0530

    nchabge2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 // import CodeSplit from "./InterviewPrep/CodeSplit/CodeSplit";
 import React, { Suspense } from 'react';
 import TableExample from "./TableExample";
+import CustomCarousel from "./playFiles/CustomCarousel/CustomCarousel";
 function App() {
 
   return (
     <div className="App">
-    <TableExample/>
+    {/* <TableExample/> */}
+    <CustomCarousel/>
     </div>
   );
 }
---
+++ src/playFiles/CustomCarousel/CustomCarousel.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CustomCarousel = () => {
+  return (
+    <div style={{
+        width: "400px",
+        height: "600px",
+        
+        backgroundColor: "red"
+    }}>
+      
+    </div>
+  )
+}
+
+export default CustomCarousel
