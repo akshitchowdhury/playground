91e997167329dfc17da7b53d8be287ab224f30fd
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-08 23:46:53 +0530

    nchabge12

--- src/playFiles/Table/Table.jsx
+++ src/playFiles/Table/Table.jsx
@@ -6,13 +6,25 @@ const Table = () => {
     const depts = new Set (data.map((item)=>item.dept))
     const[deptList,setDeptList] = useState([])
     
-     
+     const[objectList,setObjectList] = useState([{name: '', dept:''}])
     useEffect(()=>{
         // setDataList(data)
         // setDeptList(data.map((item)=>item.dept))
         console.log(deptList)
         setDataList(data)
         setDeptList([...depts])
+
+        const groupedList = [...depts].map((dept)=>{
+            const people = data.filter((item)=>item.dept===dept);
+            return{
+                dept: dept,
+                people: people.map((person)=>({
+                    name: person.name,
+                    email: person.email
+                }))
+            }
+        })
+    setObjectList(groupedList)
     },[])
   return (
     <div>
