9ef8a2c6cd2f0624f283b316e1049b467aaf066e
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-08 22:44:35 +0530

    nchabge7

---
+++ src/playFiles/Table/data.js
@@ -0,0 +1,94 @@
+const data = [
+    {
+      id: 1,
+      name: "John Doe",
+      email: "john.doe@example.com",
+      dept: "Marketing",
+      status: "Active",
+      progress: 78,
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      email: "jane.smith@example.com",
+      dept: "Sales",
+      status: "Active",
+      progress: 85,
+    },
+    {
+      id: 3,
+      name: "Alice Johnson",
+      email: "alice.johnson@example.com",
+      dept: "Finance",
+      status: "Inactive",
+      progress: 62,
+    },
+    {
+      id: 4,
+      name: "Bob Brown",
+      email: "bob.brown@example.com",
+      dept: "IT",
+      status: "Active",
+      progress: 90,
+    },
+    {
+      id: 5,
+      name: "Charlie Davis",
+      email: "charlie.davis@example.com",
+      dept: "HR",
+      status: "Inactive",
+      progress: 54,
+    },
+    {
+      id: 6,
+      name: "Emily White",
+      email: "emily.white@example.com",
+      dept: "Marketing",
+      status: "Active",
+      progress: 70,
+    },
+    {
+      id: 7,
+      name: "Frank Moore",
+      email: "frank.moore@example.com",
+      dept: "Sales",
+      status: "Inactive",
+      progress: 45,
+    },
+    {
+      id: 8,
+      name: "Grace Lee",
+      email: "grace.lee@example.com",
+      dept: "Finance",
+      status: "Active",
+      progress: 88,
+    },
+    {
+      id: 9,
+      name: "Harry Wilson",
+      email: "harry.wilson@example.com",
+      dept: "IT",
+      status: "Inactive",
+      progress: 34,
+    },
+    {
+      id: 10,
+      name: "Ivy King",
+      email: "ivy.king@example.com",
+      dept: "HR",
+      status: "Active",
+      progress: 95,
+    },
+    {
+      id: 11,
+      name: "Johnny Dan",
+      email: "johnny.dan@example.com",
+      dept: "ITC",
+      status: "Active",
+      progress: 37,
+    }
+    // ... continue adding up to 50 objects
+  ];
+  
+  export default data;
+  
