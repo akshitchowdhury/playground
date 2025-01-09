901debecfee2a47201d14a1320dcf8eae493674f
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-10 01:32:30 +0530

    Todo2

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import React, { Suspense } from 'react';
 import TableExample from "./TableExample";
 import CustomCarousel from "./playFiles/CustomCarousel/CustomCarousel";
 import Table from "./playFiles/Table/Table";
+import Todo from "./playFiles/ToDoApp/Todo";
 function App() {
 
   return (
     <div className="App">
     {/* <TableExample/> */}
     {/* <Table/> */}
+    <Todo/>
+    
     </div>
   );
 }
--- src/playFiles/ToDoApp/Todo.jsx
+++ src/playFiles/ToDoApp/Todo.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Todo = () => {
-  return (
+    const[taskList,setTaskList] = useState([])
+
+    const[taskName,setTaskName] = useState('')
+    const handleSubmission = (event)=>{
+            event.preventDefault();
+            console.log("New task added")
+            
+            setTaskList([...taskList,taskName])    
+            console.log(taskList) 
+            setTaskName('')
+    }
+
+    return (
+        <>
     <div>
-      
+      <form >
+        <input type='text' onChange={(e)=> setTaskName(e.target.value) } value={taskName} placeholder='Enter task name'/>
+        <button onClick={handleSubmission}>Add task</button>
+      </form>
     </div>
+
+        {
+            taskList.length>0 && taskList.map((task,index)=>(
+                <p key={index}>{task}</p>
+            ))
+        }
+
+    </>
   )
 }
 
