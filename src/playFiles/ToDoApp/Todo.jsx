9cab56b1139bb146488a17faa6f47dee2ca450d6
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-10 03:01:55 +0530

    Todo4

--- src/playFiles/ToDoApp/Todo.jsx
+++ src/playFiles/ToDoApp/Todo.jsx
@@ -1,44 +1,87 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const Todo = () => {
-    const[taskList,setTaskList] = useState([])
+  const [taskList, setTaskList] = useState([]);
+  const [taskName, setTaskName] = useState('');
+  const [editingTask, setEditingTask] = useState(null);
 
-    const[taskName,setTaskName] = useState('')
-    const handleSubmission = (event)=>{
-            event.preventDefault();
-            console.log("New task added")
-            
-            setTaskList([...taskList,taskName])    
-            console.log(taskList) 
-            setTaskName('')
-    }
+  const handleSubmission = (event) => {
+    event.preventDefault();
+    setTaskList([...taskList, taskName]);
+    setTaskName('');
+  };
 
-    const handleDeleteTask = (currtask)=>{
-        setTaskList(taskList.filter((task)=>currtask!==task))
-        console.log("Task deleted!")
-    }
-    return (
-        <>
-    <div>
-      <form >
-        <input type='text' onChange={(e)=> setTaskName(e.target.value) } value={taskName} placeholder='Enter task name'/>
-        <button onClick={handleSubmission}>Add task</button>
-      </form>
-    </div>
+  const handleDeleteTask = (currTask) => {
+    setTaskList(taskList.filter((task) => task !== currTask));
+  };
 
-        {
-            taskList.length>0 && taskList.map((task,index)=>(
-                <>
-                <div key={index} className='flex flex-row'> 
-                <p>{task}</p>
-                <button onClick={()=>handleDeleteTask(task) } className='bg-red-500 border-2 border-blue-500 rounded-md'>Delete task</button>
-                </div>
-                </>
-            ))
-        }
+  const handleEdit = (task) => {
+    setEditingTask(task);
+    setTaskName(task);
+  };
 
+  const handleUpdateTask = (index) => {
+    setTaskList(
+      taskList.map((item, idx) => (idx === index ? taskName : item))
+    );
+    setEditingTask(null);
+    setTaskName('');
+  };
+
+  return (
+    <>
+      <div>
+        <form>
+          <input
+            type="text"
+            onChange={(e) => setTaskName(e.target.value)}
+            value={taskName}
+            placeholder="Enter task name"
+          />
+          <button onClick={handleSubmission}>Add task</button>
+        </form>
+      </div>
+
+      {taskList.length > 0 &&
+        taskList.map((task, index) => (
+          <div key={index} className="flex flex-row justify-evenly p-6">
+            {editingTask === task ? (
+              <>
+                <input
+                  className="border-2 border-black"
+                  onChange={(e) => setTaskName(e.target.value)}
+                  value={taskName}
+                  type="text"
+                />
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleUpdateTask(index);
+                  }}
+                >
+                  Update Task
+                </button>
+              </>
+            ) : (
+              <p>{task}</p>
+            )}
+
+            <button
+              onClick={() => handleEdit(task)}
+              className="bg-green-500 border-2 border-blue-500 rounded-md"
+            >
+              Edit Task
+            </button>
+            <button
+              onClick={() => handleDeleteTask(task)}
+              className="bg-red-500 border-2 border-blue-500 rounded-md"
+            >
+              Delete Task
+            </button>
+          </div>
+        ))}
     </>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
