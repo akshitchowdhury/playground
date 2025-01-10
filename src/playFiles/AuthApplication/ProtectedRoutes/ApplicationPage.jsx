6440bf88cee09240411746a3c9272eb3451dbcf5
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-11 01:51:32 +0530

    AuthApp5

--- src/playFiles/AuthApplication/ProtectedRoutes/ApplicationPage.jsx
+++ src/playFiles/AuthApplication/ProtectedRoutes/ApplicationPage.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const ApplicationPage = () => {
+    const [taskName,setTaskName] = useState('')
+    const[taskList,setTaskList] = useState([])
+    const handleAddTask = (event)=>{
+        event.preventDefault();
+        setTaskList([...taskList,taskName])
+    }
     const navigate = useNavigate()
     const handleLogout = ()=>{
         localStorage.removeItem("auth")
@@ -9,8 +15,18 @@ const ApplicationPage = () => {
     }
   return (
     <div>
-      Succesfully logged in
-      <button onClick={handleLogout}>Log Out</button>
+    <button onClick={handleLogout}>Log Out</button>
+    <input onChange={(e)=>setTaskName(e.target.value)} value={taskName} type='text' placeholder='Enter task name'/>
+    <button onClick={(e)=>handleAddTask(e)}>Add task</button>
+      {
+        taskList.map((task,index)=>(
+            <>
+                <p key={index}>{task}</p>
+            </>
+        ))
+      }
+
+      
     </div>
   )
 }
