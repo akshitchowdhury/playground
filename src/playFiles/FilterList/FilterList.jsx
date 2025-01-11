ed1e9d150829de92e0108ed4c8b18e44996e8af6
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-12 01:46:56 +0530

    FilterList3

--- src/playFiles/FilterList/FilterList.jsx
+++ src/playFiles/FilterList/FilterList.jsx
@@ -10,10 +10,14 @@ const FilterList = () => {
             setAnimeList(data.filter((anime)=>anime.toLowerCase().includes(searchAnime.toLowerCase())))
     },[searchAnime])
 
+    const handleFilterWithA = ()=>{
+        setAnimeList(data.filter((anime)=>anime.toLowerCase().charAt(0)==="a"))
+    }
     // const fileterdAnimeList = setAnimeList(animeList.filter((anime)=>anime===searchAnime.toLowerCase()))
     return (
     <div>
-
+    <button onClick={handleFilterWithA}>Filter anime with "A"</button>
+    <button onClick={()=>setAnimeList(data)}>Reset List</button>
     <input type='text' value={searchAnime} onChange={(e)=>setSearchAnime(e.target.value)} placeholder='Search Anime'/>
             {
                 animeList.map((anime,index)=>(<>
