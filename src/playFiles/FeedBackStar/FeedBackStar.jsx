96d9c685e945e51805a68103134b3f4716e14dd7
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-11 21:15:39 +0530

    feedbackStar2

--- src/playFiles/FeedBackStar/FeedBackStar.jsx
+++ src/playFiles/FeedBackStar/FeedBackStar.jsx
@@ -1,12 +1,22 @@
+import { hover } from '@testing-library/user-event/dist/hover'
 import React, { useState } from 'react'
 
 const FeedBackStar = () => {
-    const[isHovered,setIsHovered]= useState(false)
+    
+    const[hoverIndex,setHoverIndex] = useState(null)
+    const handleEntryHover = (ind)=>{
+        setHoverIndex(ind);
+        
+    }
   return (
     <>
-    <div onMouseEnter={()=>setIsHovered(!isHovered)} onMouseLeave={()=>setIsHovered(!isHovered)}
-     className={`w-[100px] h-[50px] rounded-md border-[2px] border-black ${isHovered ? "bg-amber-500" : "" }`}>
-      
+    {/*  onMouseLeave={()=> hoverIndex===4 ? setHoverIndex(null) : setHoverIndex(index)} */}
+    <div className='flex flex-row'>
+    {
+        Array.from({length:5}).map((_,index)=><div  key={index} 
+        onMouseEnter={()=>handleEntryHover(index)} onMouseLeave={()=>setHoverIndex(null)}
+        className={`w-[100px] h-[50px]  rounded-md border-[2px] border-black ${hoverIndex>=index ? "bg-amber-500" : "" }`}></div>)
+    }
     </div>
     </>
   )
