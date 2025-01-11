485c94e7340fd58ea7a0bea70573569cebc6e5af
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-12 01:43:55 +0530

    FilterList2

--- src/playFiles/FilterList/FilterList.jsx
+++ src/playFiles/FilterList/FilterList.jsx
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react'
-import { data } from 'react-router-dom'
+import data from './data'
 
 const FilterList = () => {
 
     const[animeList,setAnimeList] = useState([])
+    const[searchAnime,setSearchAnime] = useState("")
 
     useEffect(()=>{
-            setAnimeList(data)
-    },[])
+            setAnimeList(data.filter((anime)=>anime.toLowerCase().includes(searchAnime.toLowerCase())))
+    },[searchAnime])
 
+    // const fileterdAnimeList = setAnimeList(animeList.filter((anime)=>anime===searchAnime.toLowerCase()))
     return (
     <div>
+
+    <input type='text' value={searchAnime} onChange={(e)=>setSearchAnime(e.target.value)} placeholder='Search Anime'/>
             {
                 animeList.map((anime,index)=>(<>
                     <div key={index}>
