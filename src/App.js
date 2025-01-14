030dbf744f3895e0092ac350d307a0d83e48527b
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-14 22:02:22 +0530

    paginationTest

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
       <Route path="/" element={<Login/>}/>
       </Routes>
       </Router> */}
-      <FeedBackStar/>
+      {/* <FeedBackStar/> */}
       <FilterList/>
     </div>
   );
--- src/playFiles/FilterList/FilterList.jsx
+++ src/playFiles/FilterList/FilterList.jsx
@@ -1,32 +1,59 @@
+// import React, { useEffect, useState } from 'react'
+// import data from './data'
+
+// const FilterList = () => {
+
+//     const[animeList,setAnimeList] = useState([])
+//     const[searchAnime,setSearchAnime] = useState("")
+
+//     useEffect(()=>{
+//             setAnimeList(data.filter((anime)=>anime.toLowerCase().includes(searchAnime.toLowerCase())))
+//     },[searchAnime])
+
+//     const handleFilterWithA = ()=>{
+//         setAnimeList(data.filter((anime)=>anime.toLowerCase().charAt(0)==="a"))
+//     }
+//     // const fileterdAnimeList = setAnimeList(animeList.filter((anime)=>anime===searchAnime.toLowerCase()))
+//     return (
+//     <div>
+//     <button onClick={handleFilterWithA}>Filter anime with "A"</button>
+//     <button onClick={()=>setAnimeList(data)}>Reset List</button>
+//     <input type='text' value={searchAnime} onChange={(e)=>setSearchAnime(e.target.value)} placeholder='Search Anime'/>
+//             {
+//                 animeList.map((anime,index)=>(<>
+//                     <div key={index}>
+//                         <p>{anime}</p>
+//                     </div>
+//                 </>))
+//             }      
+//     </div>
+//   )
+// }
+
+// export default FilterList
+
+
 import React, { useEffect, useState } from 'react'
 import data from './data'
 
 const FilterList = () => {
-
     const[animeList,setAnimeList] = useState([])
-    const[searchAnime,setSearchAnime] = useState("")
 
     useEffect(()=>{
-            setAnimeList(data.filter((anime)=>anime.toLowerCase().includes(searchAnime.toLowerCase())))
-    },[searchAnime])
-
-    const handleFilterWithA = ()=>{
-        setAnimeList(data.filter((anime)=>anime.toLowerCase().charAt(0)==="a"))
-    }
-    // const fileterdAnimeList = setAnimeList(animeList.filter((anime)=>anime===searchAnime.toLowerCase()))
-    return (
+        setAnimeList(data)
+    },[])
+  return (
+    <>
     <div>
-    <button onClick={handleFilterWithA}>Filter anime with "A"</button>
-    <button onClick={()=>setAnimeList(data)}>Reset List</button>
-    <input type='text' value={searchAnime} onChange={(e)=>setSearchAnime(e.target.value)} placeholder='Search Anime'/>
-            {
-                animeList.map((anime,index)=>(<>
-                    <div key={index}>
-                        <p>{anime}</p>
-                    </div>
-                </>))
-            }      
+      {
+        animeList.map((anime,index)=>(<>
+            <p key={index}>
+                {anime}
+            </p>
+        </>))
+      }
     </div>
+    </>
   )
 }
 
