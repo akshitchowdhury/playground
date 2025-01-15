08963e8b1692198212f2ff0fd68471bac8724e5a
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-16 01:00:19 +0530

    animePorta4

--- src/playFiles/AnimePortal/AnimePortal.jsx
+++ src/playFiles/AnimePortal/AnimePortal.jsx
@@ -4,14 +4,16 @@ import { useState } from 'react'
 import animeData from './data'
 
 const AnimePortal = () => {
+    const[animeStack,setAnimeStack] = useState([])
     const[animeList,setAnimeList] = useState([])
     const[animeCategories,setAnimeCategories] = useState([])
     const animeRender = ()=>{
      const animeTitles =  animeData.data.categories.flatMap((category)=> category.animeStack.map((anime)=>anime.title))
-
+        
         setAnimeCategories(animeData.data.categories)
         setAnimeList(animeTitles)
-        
+        setAnimeStack(animeData.data.categories.map((item)=>item.animeStack))
+        console.log(animeStack)
     }
 
     useEffect(()=>{
@@ -28,16 +30,18 @@ const AnimePortal = () => {
             </>
         ))
       }
-
-      {/* {
-        animeList.map((anime,index)=>(
-            <>
-                <p key={index}>
-                    {anime}
-                </p>
-            </>
+      {
+        animeStack.flatMap((stack,index)=>(
+            stack.map((block)=>(
+                <>
+                    <p>
+                        {block.title}
+                    </p>
+                </>
+            ))
         ))
-      } */}
+      }
+      
     </div>
   )
 }
