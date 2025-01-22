8f6758818e2dd1f77c80af3628fe963df18af3c8
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-22 20:00:29 +0530

    imgCarousel2

--- src/playFiles/CustomCarousel/ImageCarousel/ImageCarousel.jsx
+++ src/playFiles/CustomCarousel/ImageCarousel/ImageCarousel.jsx
@@ -3,23 +3,26 @@ import React, { useEffect, useState } from 'react'
 const ImageCarousel = () => {
     const[productList,setProductList] = useState([])
     const[pordImages,setProdImages] = useState([])
+    
     const getProducts = async()=>{
         const response = await fetch("https://dummyjson.com/products",{method: "GET"})
         const data = await response.json()
         setProductList(data.products)
 
-        setProdImages(data.products.forEach((product)=>product.images))
+        setProdImages(data.products.flatMap((product)=>product.images)) 
 
+        // setCarousleImgList(pordImages.flatMap((imgArray)=> imgArray))
         console.log("Product list", productList)
         console.log("Images arr", pordImages)
-    }
+        
+    } 
 
     useEffect(()=>{
         getProducts()
     },[])
   return (
     <div>
-      {
+      {/* {
         productList.map((item,index)=>(
             <>
                 <p key={index}>
@@ -28,7 +31,16 @@ const ImageCarousel = () => {
                 <img className='h-[300px] w-[200px]' src={item.images}></img>
             </>
         ))
+      } */}
+    <div className='grid grid-cols-3'>
+      {
+        pordImages.map((img,index)=>(
+            <>
+                <img className='h-[400px] w-[200px]' key={index} src={img}/>
+            </>
+        ))
       }
+      </div>
     </div>
   )
 }
