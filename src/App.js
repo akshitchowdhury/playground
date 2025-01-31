9b390d53f52b3399d68bcadebc063093a6db2ea2
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-02-01 00:57:10 +0530

    IcTwo3

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
       </Router> */}
       {/* <FeedBackStar/> */}
       {/* <AnimePortal/> */}
-      <IC_Two/>
+      <IC_Two/> 
     </div>
   );
 }
--- src/playFiles/CustomCarousel/ImageCarousel/IC_Two.jsx
+++ src/playFiles/CustomCarousel/ImageCarousel/IC_Two.jsx
@@ -2,19 +2,31 @@ import React, { useEffect, useState } from 'react'
 
 const IC_Two = () => {
     const[imageList,setImageList] = useState([])
+    const[productList,setProductList] = useState([])
     const fetchImages = async () => {
         const response = await fetch("https://dummyjson.com/products",{method: "GET"})
-        const data = response.json();
-        setImageList(data.products.images)
-        console.log(imageList)
+        const data = await response.json();
+        setProductList(data.products)
+        setImageList(productList.flatMap((item)=>item.images))
+        // setImageList(data.products.flatmap((item)=>item.images))    
+        console.log("Product list", productList)
+        console.log("Image list",imageList) 
     }
 
     useEffect(()=>{
         fetchImages()
-    },[])
+    },[]) 
   return (
-    <div>
-      
+    <div className='grid grid-cols-3 p-4'>
+      {
+        imageList.map((image,idx)=>(
+            <>
+            <div className='flex flex-row gap-3' key={idx}>
+                <img src={image} className='h-[200px] w-[100px]'/>
+                </div>
+            </>
+        ))
+      }
     </div>
   )
 }
